2071b00f9239b6e186391c962800d56a3fdc9683
Giovanni Braconi <thumpback2002.it@gmail.com>
2023-03-07 13:31:06 +0100

    Added twoplayer controller and colliders to ballons in the Level scene

---
+++ Assets/Scripts/TwoPlayerInput.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoPlayerInput : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private GameObject Player1;
+    [SerializeField]
+    private GameObject Player2;
+    private float speed = 0.04f;
+
+    private void Movements()
+    {
+
+        if (Player1 && Player2)
+        {
+
+            Player1.transform.position = new Vector3(Player1.transform.position.x + Input.GetAxis("HorizontalPlayer1") * speed, Player1.transform.position.y, Player1.transform.position.z);
+
+            Player1.transform.position = new Vector3(Player1.transform.position.x, Player1.transform.position.y + Input.GetAxis("VerticalPlayer1") * speed, Player1.transform.position.z);
+
+            Player2.transform.position = new Vector3(Player2.transform.position.x + Input.GetAxis("HorizontalPlayer2") * speed, Player2.transform.position.y, Player2.transform.position.z);
+
+            Player2.transform.position = new Vector3(Player2.transform.position.x, Player2.transform.position.y + Input.GetAxis("VerticalPlayer2") * speed, Player2.transform.position.z);
+        }
+    }
+    void Update()
+    {
+        Movements();
+    }
+}
